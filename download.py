28e9caf0cc37bf0310a833a5f369fd5648f59b87
Rishabh <Rishabh9843@gmail.com>
2021-02-11 12:36:31 +0530

    Downloading Images and pdf combining images

--- download.py
+++ download.py
@@ -2,11 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from concurrent.futures import ThreadPoolExecutor
+from fpdf import FPDF
 
 
-def download_slides(image_link):
+def download_slides(image_link, index):
     res = requests.get(image_link['data-full'])
-    file_name = image_link['data-full'].split("?")[0].split("/")[-1]
+    file_name = image_link['data-full'].split("/")[-1].split("?")[0]
+    print(file_name)
     with open(file_name, 'wb') as image_file:
         image_file.write(res.content)
 
@@ -23,4 +25,15 @@ soup = BeautifulSoup(res.text, 'lxml')
 images_links = soup.select('.slide_image')
 
 with ThreadPoolExecutor(max_workers=5) as executor:
-    executor.map(download_slides, images_links)
+    for index in range(1, len(images_links)+1):
+        executor.submit(download_slides, images_links[index-1], index)
+
+
+pdf = FPDF()
+image_list = os.listdir()
+for image in (sorted(image_list, key=lambda x: int(x.split("-")[2]))):
+    pdf.add_page()
+    pdf.image(image, 0, 0, 200, 200)
+os.chdir("..")
+pdf_name = link.split("/")[4].split("?")[0]
+pdf.output(pdf_name+".pdf", "F")
