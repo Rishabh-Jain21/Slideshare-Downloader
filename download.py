d66e582ada733f5f34c0adf8c3c3ad148f9b93d9
Rishabh Jain <rishabh9843@gmail.com>
2023-04-15 22:34:45 +0530

    Updated the code to fetch images correctly

--- download.py
+++ download.py
@@ -2,38 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from concurrent.futures import ThreadPoolExecutor
-from fpdf import FPDF
 
 
-def download_slides(image_link, index):
-    res = requests.get(image_link['data-full'])
-    file_name = image_link['data-full'].split("/")[-1].split("?")[0]
-    print(file_name)
+def download_slides(image_link):
+    image_link = image_link.split("?")[0]
+    res = requests.get(image_link.split("?")[0])
+    file_name = image_link.split("/")[-1].split("?")[0]
+
     with open(file_name, 'wb') as image_file:
         image_file.write(res.content)
 
 
 link = input("Enter url of the page: ")
-#link = "https://www.slideshare.net/MohamedTalaat9/digital-watermarking-91878439?qid=3c79fb40-c68e-468f-bc6e-fde768d5fb2e&v=&b=&from_search=9"
+# link = "https://www.slideshare.net/MohamedTalaat9/digital-watermarking-91878439?qid=3c79fb40-c68e-468f-bc6e-fde768d5fb2e&v=&b=&from_search=9"
 
-
-if not os.path.exists(link.split("/")[4].split("?")[0]):
-    os.mkdir(link.split("/")[4].split("?")[0])
-os.chdir(link.split("/")[4].split("?")[0])
+folder_path = link.split("/")[4].split("?")[0]
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
+os.chdir(folder_path)
 res = requests.get(link)
 soup = BeautifulSoup(res.text, 'lxml')
-images_links = soup.select('.slide_image')
+images_links = soup.select('#slide-container')
+print(type(images_links))
+urls = images_links[0].find_all('source')
+
 
 with ThreadPoolExecutor(max_workers=5) as executor:
-    for index in range(1, len(images_links)+1):
-        executor.submit(download_slides, images_links[index-1], index)
-
-
-pdf = FPDF()
-image_list = os.listdir()
-for image in (sorted(image_list, key=lambda x: int(x.split("-")[2]))):
-    pdf.add_page()
-    pdf.image(image, 0, 0, 200, 200)
-os.chdir("..")
-pdf_name = link.split("/")[4].split("?")[0]
-pdf.output(pdf_name+".pdf", "F")
+    for url in urls:
+        image_url = (url.get("srcset").split()[-2])
+        executor.submit(download_slides, image_url)
